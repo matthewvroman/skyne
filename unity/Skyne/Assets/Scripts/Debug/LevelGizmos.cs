5ad1008dea8a79cf82f666f0df7d18eaa7979422
Kevin Adams <kadams8670@gmail.com>
2017-02-17 11:14:34 -0800

    Fixed error with letter and number icon gizmos mixed up.

--- unity/Skyne/Assets/Scripts/Debug/LevelGizmos.cs
+++ unity/Skyne/Assets/Scripts/Debug/LevelGizmos.cs
@@ -9,15 +9,17 @@ public class LevelGizmos : MonoBehaviour
 	public float gridEdgeSize; 
 	public bool drawCorners; 
 
+	public bool drawGridLabels; 
+
 	void OnDrawGizmos() 
 	{
 		Gizmos.color = Color.yellow;
 
 		//Gizmos.DrawWireSphere(new Vector3(0, 0, 0), 5);
 
-		for (int column = 0; column < numColumns; column++)
+		for (int column = 0; column < numColumns + 1; column++)
 		{
-			for (int row = 0; row < numRows; row++)
+			for (int row = 0; row < numRows + 1; row++)
 			{
 				if (drawCorners)
 				{
@@ -28,6 +30,27 @@ public class LevelGizmos : MonoBehaviour
 				}
 			}
 		}
+
+		if (drawGridLabels)
+		{
+			for (int column = 1; column <= numColumns; column++)
+			{
+				Gizmos.DrawIcon(new Vector3((column-1) * gridEdgeSize + (gridEdgeSize/2), 0, -12), "label-" + ((char)(column + 64)) + ".png", true);
+			}
+
+			for (int row = 1; row <= numRows; row++)
+			{
+				
+				Gizmos.DrawIcon(new Vector3(-12, 0, (row-1) * gridEdgeSize + (gridEdgeSize/2)), "label-" + row + ".png", true);
+			}
+
+			// Levels
+			Gizmos.DrawIcon(new Vector3(-12, 16, -12), "label-" + 1 + ".png", true);
+			Gizmos.DrawIcon(new Vector3(-12, 48, -12), "label-" + 2 + ".png", true);
+			Gizmos.DrawIcon(new Vector3(-12, 80, -12), "label-" + 3 + ".png", true);
+
+		}
+
 			
 	}
 }
