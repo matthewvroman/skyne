d42de040516120a3af19c7da4ff56fb22e87c71d
Joshua Estill <jestill@mail.bradley.edu>
2017-02-16 20:37:51 -0800

    Unity folder test update

---
+++ unity/Skyne/Assets/Scripts/Managers/MainCameraControl.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraControl : MonoBehaviour 
+{
+	public Transform player;                                           // Player's reference.
+	public Vector3 pivotOffset = new Vector3(0.0f, 1.0f,  0.0f);       // Offset to repoint the camera.
+	public Vector3 cameraOffset   = new Vector3(0.0f, 0.7f, -3.0f);       // Offset to relocate the camera related to the player position.
+	public float smooth = 10f;                                         // Speed of camera responsiveness.
+	public float horizontalAimingSpeed = 400f;                         // Horizontal turn speed.
+	public float verticalAimingSpeed = 400f;                           // Vertical turn speed.
+	public float maxVerticalAngle = 30f;                               // Camera max clamp angle. 
+	public float minVerticalAngle = -60f;                              // Camera min clamp angle.
+
+	private float HorizontalAngle = 0;                                 // Float to store camera horizontal angle related to mouse movement.
+	private float VerticalAngle = 0;                                   // Float to store camera vertical angle related to mouse movement.
+	private Transform camera;                                          // This transform.
+	private Vector3 relativePositionToPlayer;                          // Current camera position relative to the player.
+	private float relativeDistanceToPlayer;                            // Current camera distance to the player.
+	private Vector3 smoothPivotOffset;                                 // Camera current pivot offset on interpolation.
+	private Vector3 smoothCamOffset;                                   // Camera current offset on interpolation.
+	private Vector3 targetPivotOffset;                                 // Camera pivot offset target to iterpolate.
+	private Vector3 targetCamOffset;                                   // Camera offset target to interpolate.
+	private float defaultFieldOfView;                                  // Default camera Field of View.
+	private float targetFieldOfView;                                   // Target camera FIeld of View.
+	private float verticalAngleClamp;                              	   // Custom camera max vertical clamp angle. 
+
+	void Awake()
+	{
+		// Reference to the camera transform.
+		camera = transform;
+
+		// Set camera default position.
+		camera.position = player.position + Quaternion.identity * pivotOffset + Quaternion.identity * cameraOffset; 
+		camera.rotation = Quaternion.identity;
+
+		// Get camera position relative to the player, used for collision test.
+		relativePositionToPlayer = transform.position - player.position;
+		relativeDistanceToPlayer = relativePositionToPlayer.magnitude - 0.5f;
+
+		// Set up references and default values.
+		smoothPivotOffset = pivotOffset;
+		smoothCamOffset = cameraOffset;
+		defaultFieldOfView = camera.GetComponent<Camera>().fieldOfView;
+
+		ResetTargetOffsets ();
+		ResetFieldOfView ();
+		ResetMaxVerticalAngle();
+	}
+
+	void Update() {
+		
+	}
+
+	void FixedUpdate() 
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void LateUpdate()
+	{
+		// Get mouse movement to orbit the camera.
+		HorizontalAngle += Mathf.Clamp(Input.GetAxis("Mouse X"), -1, 1) * horizontalAimingSpeed * Time.deltaTime;
+		VerticalAngle += Mathf.Clamp(Input.GetAxis("Mouse Y"), -1, 1) * verticalAimingSpeed * Time.deltaTime;
+
+		// Set vertical movement limit.
+		VerticalAngle = Mathf.Clamp(VerticalAngle, minVerticalAngle, verticalAngleClamp);
+
+		// Set camera orientation..
+		Quaternion camYRotation = Quaternion.Euler(0, HorizontalAngle, 0);
+		Quaternion aimRotation = Quaternion.Euler(-VerticalAngle, HorizontalAngle, 0);
+		camera.rotation = aimRotation;
+
+		// Set FOV.
+		camera.GetComponent<Camera>().fieldOfView = Mathf.Lerp (camera.GetComponent<Camera>().fieldOfView, targetFieldOfView,  Time.deltaTime);
+
+		// Test for collision with the environment based on current camera position.
+		Vector3 baseTempPosition = player.position + camYRotation * targetPivotOffset;
+		Vector3 noCollisionOffset = targetCamOffset;
+		for(float zOffset = targetCamOffset.z; zOffset <= 0; zOffset += 0.5f)
+		{
+			noCollisionOffset.z = zOffset;
+			if (DoubleViewingPosCheck (baseTempPosition + aimRotation * noCollisionOffset) || zOffset == 0) 
+			{
+				break;
+			} 
+		}
+
+		// Repostition the camera.
+		smoothPivotOffset = Vector3.Lerp(smoothPivotOffset, targetPivotOffset, smooth * Time.deltaTime);
+		smoothCamOffset = Vector3.Lerp(smoothCamOffset, noCollisionOffset, smooth * Time.deltaTime);
+
+		camera.position =  player.position + camYRotation * smoothPivotOffset + aimRotation * smoothCamOffset;
+	}
+
+	// Set camera offsets to custom values.
+	public void SetTargetOffsets(Vector3 newPivotOffset, Vector3 newCamOffset)
+	{
+		targetPivotOffset = newPivotOffset;
+		targetCamOffset = newCamOffset;
+	}
+
+	// Reset camera offsets to default values.
+	public void ResetTargetOffsets()
+	{
+		targetPivotOffset = pivotOffset;
+		targetCamOffset = cameraOffset;
+	}
+
+	// Set camera vertical offset.
+	public void SetYCamOffset(float y)
+	{
+		targetCamOffset.y = y;
+	}
+
+	// Set custom Field of View.
+	public void SetFieldOfView(float customFOV)
+	{
+		this.targetFieldOfView = customFOV;
+	}
+
+	// Reset Field of View to default value.
+	public void ResetFieldOfView()
+	{
+		this.targetFieldOfView = defaultFieldOfView;
+	}
+
+	// Set max vertical camera rotation angle.
+	public void SetMaxVerticalAngle(float angle)
+	{
+		this.verticalAngleClamp = angle;
+	}
+
+	// Reset max vertical camera rotation angle to default value.
+	public void ResetMaxVerticalAngle()
+	{
+		this.verticalAngleClamp = maxVerticalAngle;
+	}
+
+	// Double check for collisions: concave objects doesn't detect hit from outside, so cast in both directions.
+	bool DoubleViewingPosCheck(Vector3 checkPos)
+	{
+		float playerFocusHeight = player.GetComponent<CapsuleCollider> ().height *0.5f;
+		return ViewingPosCheck (checkPos, playerFocusHeight) && ReverseViewingPosCheck (checkPos, playerFocusHeight);
+	}
+
+	// Check for collision from camera to player.
+	bool ViewingPosCheck (Vector3 checkPos, float deltaPlayerHeight)
+	{
+		RaycastHit hit;
+
+		// If a raycast from the check position to the player hits something...
+		if(Physics.Raycast(checkPos, player.position+(Vector3.up* deltaPlayerHeight) - checkPos, out hit, relativeDistanceToPlayer))
+		{
+			// ... if it is not the player...
+			if(hit.transform != player && !hit.transform.GetComponent<Collider>().isTrigger)
+			{
+				// This position isn't appropriate.
+				return false;
+			}
+		}
+		// If we haven't hit anything or we've hit the player, this is an appropriate position.
+		return true;
+	}
+
+	// Check for collision from player to camera.
+	bool ReverseViewingPosCheck(Vector3 checkPos, float deltaPlayerHeight)
+	{
+		RaycastHit hit;
+
+		if(Physics.Raycast(player.position+(Vector3.up* deltaPlayerHeight), checkPos - player.position, out hit, relativeDistanceToPlayer))
+		{
+			if(hit.transform != player && hit.transform != transform && !hit.transform.GetComponent<Collider>().isTrigger)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// Get camera magnitude.
+	public float getCurrentPivotMagnitude(Vector3 finalPivotOffset)
+	{
+		return Mathf.Abs ((finalPivotOffset - smoothPivotOffset).magnitude);
+	}
+}
---
+++ unity/Skyne/Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,368 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+	[System.Serializable]
+	public class MoveSettings
+	{
+		[Tooltip ("Player's forward velocity")]
+		public float forwardVel = 12;
+		[Tooltip ("Player's side velocity")]
+		public float strafeVel = 12;
+		[Tooltip ("Player's jump velocity")]
+		public float jumpVel = 25;
+		[Tooltip ("How close the player has to to the ground for them to stop falling")]
+		public float distToGrounded = 0.5f;
+		[Tooltip ("Determins which layers the player can jump off of.")]
+		public LayerMask ground;
+		[Tooltip ("Vector3 of the over-the-shoulder camera position")]
+		public Vector3 focusOffset = new Vector3(1.5f, 0f, -1f);
+	}
+
+	[System.Serializable]
+	public class PlayerSettings
+	{
+		[Tooltip ("Speed at which color attempts to complete the lerp during invuln")]
+		public float colorLerpSpeed;
+		[Tooltip ("Number of seconds that invincibility frames last")]
+		public float invulnTime;
+		[Tooltip ("Speed at which player flashes during invuln")]
+		public float flashSpeed;
+
+		[Tooltip ("The UI object representing the image for the health")]
+		public Transform healthbarFill;
+		[Tooltip ("The UI text representing the text for health")]
+		public Transform healthPercentage;
+	}
+
+	[System.Serializable]
+	public class PhysSettings
+	{
+		[Tooltip ("Player's downward acceleration when falling after a jump")]
+		public float downAccel = 0.75f;
+	}
+
+	[System.Serializable]
+	public class InputSettings
+	{
+		[Tooltip ("(Read Only) Creates a small delay between when the player presses the button and when the character responds (Read Only)")]
+		public float inputDelay = 0.1f;
+
+		[Tooltip ("(Read Only) The string reference for vertical movement")]
+		public string FORWARD_AXIS = "Vertical";
+		[Tooltip ("(Read Only) The string reference for horizontal movement")]
+		public string STRAFE_AXIS = "Horizontal";
+		[Tooltip ("(Read Only) The string reference for jumping")]
+		public string JUMP_AXIS = "Jump";
+	}
+
+	public MoveSettings moveSetting = new MoveSettings ();
+	public PhysSettings physSetting = new PhysSettings ();
+	public InputSettings inputSetting = new InputSettings ();
+	public PlayerSettings playerSetting = new PlayerSettings ();
+
+	public Camera playerCamera;
+
+	[Tooltip("Reference to the Camera controller script")]
+	public MainCameraControl camCon;
+
+	MeshRenderer playerMesh;
+	Color playerColor;
+
+	bool isOriented;
+
+	bool isInvincible = false;
+
+	Vector3 velocity = Vector3.zero;
+	Quaternion targetRotation;
+	Rigidbody rBody;
+	float forwardInput;
+	float strafeInput;
+	float jumpInput;
+
+	public static float maxHealth = 100; // The highest the players health can go.
+	static float currentHealth; // The players current health.
+	static float healthSmoothing; // Used to smooth health.
+	static float targetHealth; // Used to smooth player health up or down.
+	bool isDead = false; // Prevents the code from executing the respawn sequence multiple times.
+
+
+	/// <summary>
+	/// Shoots a raycast downwards from the player, and checks the distance between the player and the ground. If that distance is greater than the distToGrounded variable, the player will fall down
+	/// </summary>
+	bool Grounded ()
+	{
+		return Physics.Raycast (transform.position, Vector3.down, moveSetting.distToGrounded, moveSetting.ground);
+	}
+
+	void Start ()
+	{
+		targetRotation = transform.rotation;
+		rBody = GetComponent<Rigidbody> ();
+
+		playerMesh = GetComponent<MeshRenderer> ();
+		playerColor = playerMesh.material.color;
+
+		forwardInput = 0;
+		strafeInput = 0;
+		jumpInput = 0;
+
+		currentHealth = maxHealth;
+		targetHealth = maxHealth;
+	}
+
+	/// <summary>
+	/// Assignes the inputs to their respective references
+	/// </summary>
+	void GetInput ()
+	{
+		forwardInput = Input.GetAxis (inputSetting.FORWARD_AXIS); //interpolated
+		strafeInput = Input.GetAxis (inputSetting.STRAFE_AXIS); //interpolated
+		jumpInput = Input.GetAxisRaw (inputSetting.JUMP_AXIS); //non-interpolated
+
+	}
+
+	void Update ()
+	{
+		GetInput ();
+		Focus ();
+		Health ();
+
+		OrientPlayer (playerCamera);
+
+		Debug.Log (targetHealth);
+
+		if (Input.GetKeyDown (KeyCode.E)) 
+		{
+			RestoreToFullHealth ();
+		}
+
+		if (isInvincible) 
+		{
+			DamageFlash ();
+		} 
+		else 
+		{
+			playerMesh.material.color = playerColor;
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		Run ();
+		Strafe ();
+		Jump ();
+
+		rBody.velocity = transform.TransformDirection (velocity);
+	}
+
+	/// <summary>
+	/// Method that contains player's forward movememnt code
+	/// </summary>
+	void Run ()
+	{
+		if (Mathf.Abs (forwardInput) > inputSetting.inputDelay) 
+		{
+			//move
+			velocity.z = moveSetting.forwardVel * forwardInput;
+		} 
+		else 
+		{
+			//zero velocity
+			velocity.z = 0;
+		}
+	}
+
+	/// <summary>
+	/// Method that contains player's strafing code
+	/// </summary>
+	void Strafe ()
+	{
+		if (Mathf.Abs (strafeInput) > inputSetting.inputDelay) 
+		{
+			//move
+			velocity.x = moveSetting.strafeVel * strafeInput;
+		} 
+		else 
+		{
+			//zero velocity
+			velocity.x = 0;
+		}
+	}
+
+	/// <summary>
+	/// Method that contains player's jumping code
+	/// </summary>
+	void Jump ()
+	{
+		if (jumpInput > 0 && Grounded ()) 
+		{
+			//Jump
+			velocity.y = moveSetting.jumpVel;
+		} 
+		else if (jumpInput == 0 && Grounded ()) 
+		{
+			//zero out our velocity.y
+			velocity.y = 0;
+		} 
+		else 
+		{
+			//decrease velocity.y
+			velocity.y -= physSetting.downAccel;
+		}
+	}
+
+	/// <summary>
+	/// Transforms the player's rotation to that of the specified camera
+	/// </summary>
+	/// <param name="cam">Cam.</param>
+	void OrientPlayer (Camera cam)
+	{
+		if (isOriented) 
+		{
+			transform.rotation = Quaternion.Euler (transform.eulerAngles.x, cam.transform.eulerAngles.y, transform.eulerAngles.z);
+		}
+
+		if (Input.GetKey (KeyCode.Q)) 
+		{
+			isOriented = false;
+		} 
+		else 
+		{
+			isOriented = true;
+		}
+	}
+
+	/// <summary>
+	/// Moves the player camera into focus position when a button is held
+	/// </summary>
+	void Focus() 
+	{
+		if (Input.GetMouseButton (0)) 
+		{
+			camCon.SetTargetOffsets (camCon.pivotOffset, moveSetting.focusOffset);
+		} 
+		else 
+		{
+			camCon.ResetTargetOffsets ();
+		}
+	}
+
+	/// <summary>
+	/// Causes the player to become invincible for a specified number of seconds
+	/// </summary>
+	IEnumerator Invicibility() 
+	{
+		isInvincible = true;
+		yield return new WaitForSeconds (playerSetting.invulnTime);
+		isInvincible = false;
+	}
+
+	/// <summary>
+	/// Causes player model to flash red during invuln
+	/// </summary>
+	void DamageFlash() 
+	{
+		playerMesh.material.color = Color.Lerp (playerColor, Color.red, Mathf.PingPong (Time.time * playerSetting.flashSpeed, playerSetting.colorLerpSpeed));
+	}
+
+	/// <summary>
+	/// Method containing Player's health code
+	/// </summary>
+	void Health ()
+	{
+		if (currentHealth <= 0.9f && !isDead) 
+		{
+			currentHealth = 0; // This ensures that the health percentage is NEVER less than zero.
+
+			// Respawn execution goes here
+
+			isDead = true; // Prevents the code from executing the respawn sequence multiple times.
+		} 
+
+		Mathf.Round (targetHealth);
+
+		if (currentHealth >= maxHealth) 
+		{
+			currentHealth = maxHealth; // Caps the players current health to the designated max health.
+		} 
+
+		// Updates the health percentage.
+		playerSetting.healthPercentage.GetComponent<Text> ().text = ((int)currentHealth).ToString () + "%";
+
+		// Updates the health bars fill.
+		playerSetting.healthbarFill.GetComponent<Image> ().fillAmount = currentHealth / 100;
+
+
+		// Smooths the current player health value based on the target health variable.
+		currentHealth = Mathf.SmoothDamp (currentHealth, targetHealth, ref healthSmoothing, 0.3f);
+	}
+
+	/// <summary>
+	/// Heals the player by the specified amount
+	/// </summary>
+	/// <param name="healthRecieved">Health recieved.</param>
+	public static void HealCalculator (float healthRecieved)
+	{
+		// Only heals the player if player health is below the max. 
+		if (currentHealth < maxHealth) 
+		{
+			targetHealth = currentHealth + healthRecieved;
+		}
+	}
+
+	/// <summary>
+	/// Applies the specified damage value to the player's current health
+	/// </summary>
+	/// <returns>The calculator.</returns>
+	/// <param name="damageRecieved">Damage recieved.</param>
+	public static IEnumerator DamageCalculator (float damageRecieved)
+	{
+		targetHealth = currentHealth - damageRecieved;
+		yield return new WaitForSeconds (1);
+	}
+
+	/// <summary>
+	/// Restores to full health.
+	/// </summary>
+	public void RestoreToFullHealth(){
+		targetHealth = maxHealth;
+		//currentHealth = maxHealth;
+	} 
+
+	void OnCollisionEnter(Collision col) 
+	{
+		//If the player comes in contact with an enemy, initiate invincibility coroutine
+		if (col.gameObject.tag == "Enemy") 
+		{
+			//When the player collides with an enemy, it checks to see if the player is currently invincible or not. 
+			//If not, then the player will take damage
+			if (isInvincible == false) 
+			{
+				//DamageCalculator (10);
+				StartCoroutine (DamageCalculator(10));
+			}
+
+			StartCoroutine (Invicibility());
+		}
+	}
+
+	void OnCollisionStay(Collision col) 
+	{
+		//If the player is still touching the enemy when invuln wears off, ininitate invincibility coroutine again
+		if (col.gameObject.tag == "Enemy" && isInvincible == false) 
+		{
+			//When the player collides with an enemy, it checks to see if the player is currently invincible or not. 
+			//If not, then the player will take damage
+			if (isInvincible == false) 
+			{
+				//DamageCalculator (10);
+				StartCoroutine (DamageCalculator(10));
+			}
+			StartCoroutine (Invicibility());
+		}
+	}
+}
