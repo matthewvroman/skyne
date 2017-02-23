07521950b55e9d724b6b83675598a1447084a908
StephanNCG <scantero@mail.bradley.edu>
2017-02-22 21:31:59 -0800

    Added end sequence timer and working on fade transition for scene change.

---
+++ unity/Skyne/Assets/Scripts/UI Scripts/EndGameTimer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndGameTimer : MonoBehaviour {
+	public Text timerText;
+	public float minutes = 3;
+	private float seconds = 0;
+	private bool stop = false;
+
+	void Start ()
+	{
+		timerText.GetComponent<Text> ();
+		seconds = 0;
+	}
+
+	void Update ()
+	{
+		if (stop) {
+			return;
+		}
+
+		seconds -= Time.deltaTime;
+
+		if (seconds <= 0 && minutes != 0) {
+			minutes--;
+			seconds = 60;
+		}
+
+		timerText.text = minutes + ":" + seconds.ToString ("f0");
+	}
+
+	public void StopTimer ()
+	{
+		timerText.color = Color.yellow;
+		stop = true;
+	}
+}
---
+++ unity/Skyne/Assets/Scripts/UI Scripts/FadeTransition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeTransition : MonoBehaviour {
+
+	public Texture2D fadeOutTexture;
+	public float fadeSpeed = 0.8f;
+
+	private int drawDepth = -1000;
+	float alpha = 1.0f;
+	private int fadeDir = -1;
+
+	void OnGui (){
+
+	}
+}
