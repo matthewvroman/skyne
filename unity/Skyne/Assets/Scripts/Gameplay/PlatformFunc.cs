28d6a46314e6f15314fe90ff5860f2824f54dcac
Joshua Estill <jestill@mail.bradley.edu>
2017-03-09 17:26:04 -0800

    Added in platform functionality. Created a platform container prefab. Reorganized some scripts.

---
+++ unity/Skyne/Assets/Scripts/Gameplay/PlatformFunc.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFunc : MonoBehaviour
+{
+
+	MovingPlatformManager platMan;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		platMan = transform.parent.GetComponent<MovingPlatformManager> ();
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			col.transform.parent = gameObject.transform;
+		}
+	}
+
+	void OnCollisionStay (Collision col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			col.transform.parent = gameObject.transform;
+		}
+	}
+
+	void OnCollisionExit (Collision col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			col.transform.parent = null;
+		}
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.tag == "Bullet")
+		{
+			Debug.Log ("Hello");
+			platMan.SetPos (!platMan.GetSetPos ());
+		}
+	}
+
+	/*void OnTriggerStay (Collider col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			col.transform.parent = gameObject.transform;
+		}
+	}
+
+	void OnTriggerExit (Collider col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			col.transform.parent = null;
+		}
+	} */
+}
---
+++ unity/Skyne/Assets/Scripts/Managers/MovingPlatformManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformManager : MonoBehaviour
+{
+
+	GameObject platform;
+
+	Transform botPos;
+	Transform topPos;
+
+	bool setPos;
+
+	public float speed;
+	public int timer;
+
+	float countdown;
+
+	// Use this for initialization
+	void Start ()
+	{
+		platform = this.transform.FindChild ("Platform").gameObject;
+		botPos = gameObject.transform.Find ("BottomPosition");
+		topPos = gameObject.transform.Find ("TopPosition");
+
+		setPos = true;
+
+		countdown = timer;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log (countdown);
+
+		if (setPos == true)
+		{
+			countdown = timer;
+			platform.transform.position = Vector3.Lerp (platform.transform.position, botPos.position, speed * Time.smoothDeltaTime);
+		}
+
+		if (setPos == false)
+		{
+			if (countdown > 0)
+			{
+				countdown -= Time.deltaTime;
+			}
+			platform.transform.position = Vector3.Lerp (platform.transform.position, topPos.position, speed * Time.smoothDeltaTime);
+		}
+
+		if (countdown <= 0)
+		{
+			setPos = true;
+			countdown = timer;
+		}
+	}
+
+	/// <summary>
+	/// True = bottom position,
+	/// False = top position.
+	/// </summary>
+	/// <param name="b">If set to <c>true</c> b.</param>
+	public void SetPos (bool b)
+	{
+		setPos = b;
+	}
+
+	/// <summary>
+	/// Returns it's target location
+	/// True = bottom position,
+	/// False = top position.
+	/// </summary>
+	/// <param name="b">If set to <c>true</c> b.</param>
+	public bool GetSetPos ()
+	{
+		return setPos;
+	}
+}
