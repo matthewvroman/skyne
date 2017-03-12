8ddbb431c3e29a3de98a852686c4a17e5c57d2f4
Joshua Estill <jestill@mail.bradley.edu>
2017-03-12 15:15:56 -0700

    Updated Charger Manager script to contain comments and tooltips.

--- unity/Skyne/Assets/Scripts/Gameplay/Enemies/ChargerManager.cs
+++ unity/Skyne/Assets/Scripts/Gameplay/Enemies/ChargerManager.cs
@@ -5,6 +5,7 @@ using System;
 
 public class ChargerManager : Enemy
 {
+	//Lists the different possible enemy states
 	public enum State
 	{
 		IDLE,
@@ -13,19 +14,25 @@ public class ChargerManager : Enemy
 	}
 
 	public State state;
+
+	//Determines whether the enemy is alive or not. Is not currently ever changed.
 	private bool alive;
 
+	//Var holding the distance from the enemy to the player
 	float tarDistance;
 
+	[Tooltip ("The distance at which the enemy will start attacking")]
 	public float attackDistance;
+	[Tooltip ("Distance at which enemy starts moving towards player")]
 	public float aggroDistance;
-
+	[Tooltip ("Enemy's general move speed")]
 	public float moveSpeed;
+	[Tooltip ("Charger's charging speed")]
 	public float chargeSpeed;
 
-	public float damping;
-
+	[Tooltip ("The player gameobject")]
 	public Transform target;
+	[Tooltip ("This enemy's rigidbody")]
 	public Rigidbody rBody;
 
 	void Start ()
@@ -78,9 +85,10 @@ public class ChargerManager : Enemy
 
 	void FixedUpdate ()
 	{
-
+		//Determines the distance from the enemy to the player
 		tarDistance = Vector3.Distance (target.position, transform.position);
 
+		//Switches between states based on the distance from the player to the enemy
 		if (tarDistance < attackDistance)
 		{
 			state = ChargerManager.State.ATTACK;
@@ -100,12 +108,14 @@ public class ChargerManager : Enemy
 
 	}
 
+	//The Idling state, what the enemy does when the player is not close.
 	void Idle ()
 	{
 		transform.rotation = Quaternion.Euler (0, 0, 0);
 		Debug.Log ("Idling");
 	}
 
+	//The Positioning state, when the enemy first notices the player, it will get closer so that it can start attacking.
 	void Position ()
 	{
 		Vector3 targetPosition = new Vector3 (target.position.x, this.transform.position.y, target.position.z);
@@ -116,6 +126,7 @@ public class ChargerManager : Enemy
 		Debug.Log ("Positioning");
 	}
 
+	//The Attcking state, once close enough, the enemy will charge at the player.  
 	void Attack ()
 	{
 		Vector3 targetPosition = new Vector3 (target.position.x, this.transform.position.y, target.position.z);
