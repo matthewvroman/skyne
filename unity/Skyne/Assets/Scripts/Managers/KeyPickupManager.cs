565e3a0cad749f2dadcebd92fd68fa56c5ed7046
Kevin Adams <kadams@Kevins-MacBook-Pro.local>
2017-03-20 05:30:58 -0700

    Added preliminary KeyPickup and PlayerUpgradeModel scripts; Updated Player prefab with PlayerUpgradeModels script

---
+++ unity/Skyne/Assets/Scripts/Managers/KeyPickupManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPickupManager : Singleton<KeyPickupManager> 
+{
+	public GameObject keyPickupPrefab; 
+
+	public void SpawnKeyPickup(Vector3 spawnPos, int keyIndex)
+	{
+		GameObject newKeyPickup = GameObject.Instantiate(keyPickupPrefab, spawnPos, Quaternion.identity, transform); 
+	}
+}
---
+++ unity/Skyne/Assets/Scripts/Misc/PlayerUpgradeModels.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class would probably be better using an event listener, but this is the easier implementation 
+public class PlayerUpgradeModels : MonoBehaviour
+{
+	public GameObject doubleJumpModel; 
+	public GameObject wallJumpModel; 
+	public GameObject dashModel; 
+
+	public void Update()
+	{
+		if (GlobalManager.inst.GameplayIsActive())
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				if (GameState.inst.upgradesFound[i])
+				{
+					AddUpgrade(i); 
+				}
+			}
+		}
+	}
+		
+	public void AddUpgrade(int upgradeIndex)
+	{
+		if (upgradeIndex == 0 && doubleJumpModel != null)
+		{
+			doubleJumpModel.SetActive(true); 
+		}
+		else if (upgradeIndex == 1 && wallJumpModel != null)
+		{
+			wallJumpModel.SetActive(true); 
+		}
+		else if (upgradeIndex == 2 && dashModel != null)
+		{
+			dashModel.SetActive(true); 
+		}
+	}
+}
