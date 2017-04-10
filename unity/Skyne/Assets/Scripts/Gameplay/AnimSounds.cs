d942df961d8205befda9ead91a47f21763b89ab7
Joshua Estill <jestill@mail.bradley.edu>
2017-04-09 23:54:04 -0700

    Quick update

---
+++ unity/Skyne/Assets/Scripts/Gameplay/AnimSounds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimSounds : MonoBehaviour {
+
+	AudioSource animAudio;
+	public AudioClip footstep1;
+
+	// Use this for initialization
+	void Start () {
+		animAudio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void playFootstep() {
+		animAudio.PlayOneShot (footstep1);
+	}
+}
