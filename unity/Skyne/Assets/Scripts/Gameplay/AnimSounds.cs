c34eb0d6a3934a1435eba13a433f49f85cd53ebe
Joshua Estill <jestill@mail.bradley.edu>
2017-04-28 22:41:19 -0700

    Updated sounds.

--- unity/Skyne/Assets/Scripts/Gameplay/AnimSounds.cs
+++ unity/Skyne/Assets/Scripts/Gameplay/AnimSounds.cs
@@ -2,70 +2,126 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimSounds : MonoBehaviour {
+public class AnimSounds : MonoBehaviour
+{
 
 	AudioSource animAudio;
 
 	public AudioClip footstep1;
+	public AudioClip footstep2;
 
 	public AudioClip jump;
 	public AudioClip airDash;
 	public AudioClip death;
-	public AudioClip hit;
+
+	public AudioClip hit1;
+	public AudioClip hit2;
+
 	public AudioClip landing;
 	public AudioClip doubleJump;
 
-	public PlayerManager playerManager; 
+	public AudioClip wallCling;
+
+	public PlayerManager playerManager;
+
+	int count;
+	float timer = 1;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		animAudio = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	public void FootstepSFX() {
-		if (playerManager.getIsWalking() == true)
+	void Update ()
+	{
+		if (count >= 1 && timer > 0)
 		{
-			animAudio.PlayOneShot (footstep1);
+			timer -= Time.deltaTime;
+		}
+		else if (timer <= 0)
+		{
+			count = 0;
+			timer = 1;
 		}
 	}
 
-	public void JumpSFX() {
+	public void FootstepSFX ()
+	{
+		if (playerManager.getIsWalking () == true)
+		{
+			int num = Random.Range (1, 3);
+
+			switch (num)
+			{
+			case 1:
+				animAudio.PlayOneShot (footstep1);
+				break;
+
+			case 2:
+				animAudio.PlayOneShot (footstep2);
+				break;
+			}
+		}
+	}
+
+	public void JumpSFX ()
+	{
 		animAudio.PlayOneShot (jump);
 	}
 
-	public void DashSFX() {
+	public void DashSFX ()
+	{
 		animAudio.PlayOneShot (airDash);
 	}
 
-	public void DeathSFX() {
+	public void DeathSFX ()
+	{
 		animAudio.PlayOneShot (death);
 	}
 
-	public void DamageSFX() {
-		animAudio.PlayOneShot (hit);
+	public void DamageSFX ()
+	{
+		if (playerManager.GetIsAlive () == true)
+		{
+			if (count < 1)
+			{
+				int num = Random.Range (1, 3);
+
+				switch (num)
+				{
+				case 1:
+					animAudio.PlayOneShot (hit1);
+					count += 1;
+					break;
+
+				case 2:
+					animAudio.PlayOneShot (hit2);
+					count += 1;
+					break;
+				}
+			}
+		}
 	}
 
-	public void LandingSFX() 
+	public void LandingSFX ()
 	{
 		animAudio.PlayOneShot (landing);
 
 		if (playerManager != null)
 		{
-			playerManager.OnLanding(); 
+			playerManager.OnLanding (); 
 			//Debug.Log("Play landing particles"); 
 		}
 		else
 		{
-			Debug.Log("Tried to play landing particles, but playerMangager ref is null"); 
+			Debug.Log ("Tried to play landing particles, but playerMangager ref is null"); 
 		}
 	}
 
-	public void DoubleJumpSFX() {
+	public void DoubleJumpSFX ()
+	{
 		animAudio.PlayOneShot (doubleJump);
 	}
 
